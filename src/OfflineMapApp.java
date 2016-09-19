0a7afbf5b85d7ab53a19e9ff24b429424eecaa90
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-18 21:51:34 -0400

    Update OfflineMapApp.java

--- src/OfflineMapApp.java
+++ src/OfflineMapApp.java
@@ -7,7 +7,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
  * 
  * This example uses a local MBTiles file. Thus, it does not need an Internet connection to load tiles.
  */
-public class OfflineMapApp extends PApplet {
+public class OfflineMapApp extends PApplet 
+{
 
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 
