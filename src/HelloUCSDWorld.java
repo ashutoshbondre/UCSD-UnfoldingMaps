be377d61c1e51500d3f9f4604c662014236ec83d
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-01-30 00:00:02 -0500

    Update HelloUCSDWorld.java

--- src/HelloUCSDWorld.java
+++ src/HelloUCSDWorld.java
@@ -12,7 +12,7 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
  */
 public class HelloUCSDWorld extends PApplet {
 
-	UnfoldingMap map;
+	UnfoldingMap map;d
 
 	public void setup() {
 		size(800, 600, OPENGL);
